abc265d03d75e92c47519e0e9bf453d7b6b1f1e5
pallavi yewale <157051417+pallaviyewaleDefra@users.noreply.github.com>
2024-06-26 10:01:51 +0100

    dev merge

--- nipts-pts-automation-tests/obj/Debug/net8.0/nipts-pts-automation-tests.AssemblyInfo.cs
+++ nipts-pts-automation-tests/obj/Debug/net8.0/nipts-pts-automation-tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("nipts-pts-automation-tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a60971ff4f2e1a66e1b6556fb440771656ce315")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+620a5597f67d3fc536e73a7298127623903874b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("nipts-pts-automation-tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("nipts-pts-automation-tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
