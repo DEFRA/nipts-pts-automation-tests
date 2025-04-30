c7bcec5268fcf0915aa59f48f5e292b9feeb3399
pallavi yewale <157051417+pallaviyewaleDefra@users.noreply.github.com>
2025-04-30 12:21:40 +0100

    Time validation

--- nipts-pts-automation-tests/Pages/CP/Pages/WelcomePage.cs
+++ nipts-pts-automation-tests/Pages/CP/Pages/WelcomePage.cs
@@ -87,6 +87,7 @@ namespace nipts_pts_automation_tests.Pages.CP.Pages
                 departTime = headerTime.Substring(63, 5);
             }
             string matchingTime = $"//input[contains(@value,'{route}')]/following-sibling::input[contains(@value,'{departureDate}')]/following-sibling::input[contains(@value,'{departureTime}')]/..//button";
+            Console.WriteLine($"departTime : {departTime}");
             Console.WriteLine($"Matching Time: {matchingTime}");
 
             if (_driver.FindElements(By.XPath(matchingTime)).Count > 0)
@@ -123,6 +124,7 @@ namespace nipts_pts_automation_tests.Pages.CP.Pages
             }
 
             string matchingTime = $"//input[contains(@value,'{departureRoute}')]/following-sibling::input[contains(@value,'{departureDate}')]/following-sibling::input[contains(@value,'{GBDepartureTime}')]/..//button";
+            Console.WriteLine($"GBDepartureTime : {GBDepartureTime}");
             Console.WriteLine($"Matching Time: {matchingTime}");
 
             if (_driver.FindElements(By.XPath(matchingTime)).Count > 0)
