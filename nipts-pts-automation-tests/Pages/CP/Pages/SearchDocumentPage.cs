81c099ab7f4d0da0e334f2ee7dda47a8dc124ca6
pallavi yewale <157051417+pallaviyewaleDefra@users.noreply.github.com>
2025-07-24 16:21:36 +0100

    Search document label changes

--- nipts-pts-automation-tests/Pages/CP/Pages/SearchDocumentPage.cs
+++ nipts-pts-automation-tests/Pages/CP/Pages/SearchDocumentPage.cs
@@ -23,10 +23,12 @@ namespace nipts_pts_automation_tests.Pages.CP.Pages
         private IWebElement txtPTDSearchBox => _driver.WaitForElementExists(By.XPath("//input[@id='ptdNumberSearch']"));
         private IWebElement txtApplicationNumberSearchBox => _driver.WaitForElement(By.XPath("//input[@id='applicationNumberSearch']"));
         private IWebElement txtMicrochipNumberSearchBox => _driver.WaitForElement(By.XPath("//input[@id='microchipNumber']"));
-        private IWebElement expectedText => _driver.WaitForElement(By.XPath("//div[@class='ons-panel__body']"));
-        private IWebElement rdoPTDNumber => _driver.WaitForElementExists(By.XPath("//label[normalize-space()='Search by PTD number']"));
-        private IWebElement rdoApplicatioNumbere => _driver.WaitForElementExists(By.XPath("//label[normalize-space()='Search by application number']"));
-        private IWebElement rdoMicrochipNumbere => _driver.WaitForElementExists(By.XPath("//label[normalize-space()='Search by microchip number']"));
+        private By rdoPTDInput => By.XPath("//input[@id='documentSearch-1'][@aria-labelledby='documentSearch-1-label'][@aria-expanded='false']");
+        private IWebElement rdoPTDNumber => _driver.WaitForElementExists(By.XPath("//label[@id='documentSearch-1-label'][normalize-space()='Search by PTD number']"));
+        private By rdoApplicationInput => By.XPath("//input[@id='documentSearch-2'][@aria-labelledby='documentSearch-2-label'][@aria-expanded='false']");
+        private IWebElement rdoApplicatioNumbere => _driver.WaitForElementExists(By.XPath("//label[@id='documentSearch-2-label'][normalize-space()='Search by application number']"));
+        private By rdoMicrochipInput => By.XPath("//input[@id='documentSearch-3'][@aria-labelledby='documentSearch-3-label'][@aria-expanded='false']");
+        private IWebElement rdoMicrochipNumbere => _driver.WaitForElementExists(By.XPath("//label[@id='documentSearch-3-label'][normalize-space()='Search by microchip number']"));
         private IReadOnlyCollection<IWebElement> lblErrorMessages => _driver.WaitForElements(By.XPath("//div[@class='govuk-error-summary__body']//a"));
         #endregion
 
@@ -39,23 +41,29 @@ namespace nipts_pts_automation_tests.Pages.CP.Pages
         {
             if (radioButtonValue == "Search by PTD number")
             {
-                //if (!rdoPTDNumber.Selected)
-                //{
+                if (_driver.FindElements(rdoPTDInput).Count>0)
+                {
                     rdoPTDNumber.Click();
-                //}
+                }
             }
             else if (radioButtonValue == "Search by application number")
             {
                 if (!rdoApplicatioNumbere.Selected)
                 {
-                    rdoApplicatioNumbere.Click();
+                    if (_driver.FindElements(rdoApplicationInput).Count > 0)
+                    {
+                        rdoApplicatioNumbere.Click();
+                    }
                 }
             }
             else if (radioButtonValue == "Search by microchip number")
             {
                 if (!rdoMicrochipNumbere.Selected)
                 {
-                    rdoMicrochipNumbere.Click();
+                    if (_driver.FindElements(rdoMicrochipInput).Count > 0)
+                    {
+                        rdoMicrochipNumbere.Click();
+                    }
                 }
             }
         }
